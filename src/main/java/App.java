eb9514e2a99c19b31a79dec0a34744a7eb6835a4
ANDERSONROCHA81 <ironmanrocha81@gmail.com>
2022-07-21 19:04:43 -0300

    Criação da classe GeradoraDeFigurinhas e implementação do for each na classe App

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,12 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class App {
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -16,16 +17,25 @@ public class App {
         HttpRequest request = HttpRequest.newBuilder(endereco).GET().build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
+
         //Extrair só os dados que interessam(título, poster, classificação)
         JsonParser parser = new JsonParser();
         List<Map<String,String>> listaDeFilmes = parser.parse(body);
+
         //Exibir e manipular os dados
-        listaDeFilmes.forEach(filme -> {
-                    System.out.println(filme.get("title"));
-                    System.out.println(filme.get("image"));
-                    System.out.println(filme.get("imDbRating"));
-                    System.out.println();
-                });
+        GeradoraDeFigurinhas geradora = new GeradoraDeFigurinhas();
+        for (Map<String, String> filme : listaDeFilmes) {
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImagem).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            geradora.cria(inputStream, nomeArquivo);
+
+            System.out.println(titulo);
+            System.out.println();
+        }
 
     }
 }
